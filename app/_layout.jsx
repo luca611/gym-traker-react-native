1bb1c4f8a26bc79b8e7e70e0b8ccf1ab1c7bce2b
Luca Spoldi <spoldi.luca06@outlook.it>
2024-08-17 21:11:27 +0200

    setup

---
+++ app/_layout.jsx
@@ -0,0 +1,11 @@
+import {Text, View } from 'react-native'
+import React from 'react'
+import { Slot } from 'expo-router'
+
+const RootLayout = () => {
+  return (
+    <Slot/>
+  );
+}
+
+export default RootLayout
